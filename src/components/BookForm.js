5da5a8b0242c24414691969445b8b1336d88b925
Ramon Sanchez <songwright66@gmail.com>
2020-01-20 14:20:46 -0800

    Updated name of app to Ramon's Reading List

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -2,7 +2,6 @@ import React, { useContext, useState } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const NewBookForm = () => {
-  // const { addBook } = useContext(BookContext);
   const { dispatch } = useContext(BookContext);
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
